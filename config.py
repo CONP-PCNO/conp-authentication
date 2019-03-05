009cf9fe40b8feba7b2ec39599c495d913bf9979
Dawn E. Smith <Dawn.Smith@camh.ca>
2019-03-05 18:10:21 -0500

    Fixed URL (psycopg2 expects no host if localhost is used)

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ import os
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     SQLALCHEMY_DATABASE_URI = (os.environ.get('DATABASE_URL') or
-        'postgresql://localhost/conp-auth')
+        'postgresql:///conp-auth')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
